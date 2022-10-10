f497ec9ad687fe1289ea9ef8874339d428d1ee6e
ac2393921 <ac2393921@gmail.com>
2022-10-10 23:00:26 +0900

    add: tessoku 02 solver

---
+++ tessoku/02/solver.cpp
@@ -0,0 +1,25 @@
+// template for atcoder beginners
+// when you create a *.cpp file, this template is loaded
+
+#include <iostream>
+using namespace std;
+
+int N, X, A[109];
+bool Answer = false;
+
+int main() {
+  cin >> N >> X;
+  for (int i = 1; i <= N; i++)
+    cin >> A[i];
+
+  for (int i = 1; i <= N; i++) {
+    if (A[i] == X)
+      Answer = true;
+  }
+
+  if (Answer == true)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
