c9cd4038cba4ff0e1ffc30584f4c5273e448f64c
ac2393921 <ac2393921@gmail.com>
2022-11-02 00:30:13 +0900

    update: 43 solver

--- tessoku/solver.cpp
+++ tessoku/solver.cpp
@@ -3,22 +3,21 @@ using namespace std;
 #define rep(i, a, n) for (int i = (a); i < (n); ++i)
 using ll = long long;
 
-int N, A[100009];
-int dp[2009][2009];
+int N, L, A[200009];
+char B[200009];
 
 int main() {
-  cin >> N;
-  rep(i, 0, N) cin >> A[i];
+  cin >> N >> L;
+  rep(i, 0, N) cin >> A[i] >> B[i];
 
-  rep(i, 0, N) dp[N][i] = A[i];
-
-  for (int i = N - 1; i >= 0; --i) {
-    if (i % 2 == 1)
-      rep(j, 0, i) dp[i][j] = max(dp[i + 1][j], dp[i + 1][j + 1]);
-    else
-      rep(j, 0, i) dp[i][j] = min(dp[i + 1][j], dp[i + 1][j + 1]);
+  int ans = 0;
+  rep(i, 0, N) {
+    if (B[i] == 'E') {
+      ans = max(ans, L - A[i]);
+    } else {
+      ans = max(ans, A[i]);
+    }
   }
-
-  cout << dp[1][0] << endl;
+  cout << ans << endl;
   return 0;
 }
